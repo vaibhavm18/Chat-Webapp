589e2893293a50e234a35d6fade97345dd365c3f
vaibhav <vaibhavnayak018@gmail.com>
2023-11-23 19:37:24 +0530

    react redux for state management

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./state/app/store.ts";
 import "./index.css";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <App />
+        </Router>
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 );
