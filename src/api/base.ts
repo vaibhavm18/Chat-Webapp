edffc5ab171b80fc17825fb2937f815300d65981
vaibhav <vaibhavnayak018@gmail.com>
2024-02-12 19:08:18 +0530

    adding environment variable

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const API_BASE_URL = "https://social-dc1i.onrender.com/api/v1";
-const API_BASE_URL = "http://localhost:5000/api/v1";
+
+const API_BASE_URL = import.meta.env.VITE_URL || "http://localhost:5000/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
